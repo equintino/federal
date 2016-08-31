ba5683baa26fcb8a448975422375a84908375b71
Edmilson <emquintino@ig.com.br>
2016-08-31 17:03:16 -0300

    avisado

--- web/pCadastrar.php
+++ web/pCadastrar.php
@@ -5,6 +5,9 @@
     $array=unserialize($texto);
     fclose($handle);
     echo "<button onclick=history.go(-1) class=busca>VOLTAR</button>";
+    echo "<div class=avisado>";
+    echo "SINISTRO AVISADO";
+    echo "</div>";
     echo "<div id=geral>";
     echo "<table border=1 align=center cellspacing=0 spanspacing=0 class=tabela><tr><th>SINISTRO</th>";
     foreach($array as $item){
