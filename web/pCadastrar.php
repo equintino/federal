4e2378386da872d37c4d97e4a6ea92156ae69918
Casa <emquintino@ig.com.br>
2016-08-31 23:56:35 -0300

    avisado

--- web/pCadastrar.php
+++ web/pCadastrar.php
@@ -6,9 +6,15 @@
     fclose($handle);
     echo "<button onclick=history.go(-1) class=busca>VOLTAR</button>";
     echo "<div class=avisado>";
-    echo "SINISTRO AVISADO";
+    echo "<table align=center>";
+    echo "<tr><td>ANTES<img src='img/seta_esq.png' height=50px></td>";
+    echo "<td>";
+    echo "AVISADO 2011";
+    echo "</td>";
+    echo "<td><img src='img/seta_dir.png' height=50px>DEPOIS</td></tr>";
+    echo "</table>";
     echo "</div>";
-    echo "<div id=geral>";
+    echo "<div id=avisado>";
     echo "<table border=1 align=center cellspacing=0 spanspacing=0 class=tabela><tr><th>SINISTRO</th>";
     foreach($array as $item){
         echo "<tr><td>$item</td></tr>";
