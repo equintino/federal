578f651ad8ad176d5565c8ba47b13679869f2815
Edmilson <emquintino@ig.com.br>
2016-09-01 16:19:57 -0300

    avisado

--- web/pCadastrar.php
+++ web/pCadastrar.php
@@ -5,21 +5,28 @@
     $array=unserialize($texto);
     fclose($handle);
     echo "<button onclick=history.go(-1) class=busca>VOLTAR</button>";
-    echo "<div class=avisado>";
-    echo "<table align=center>";
-    echo "<tr><td>ANTES<img src='img/seta_esq.png' height=50px></td>";
-    echo "<td>";
-    echo "AVISADO 2011";
-    echo "</td>";
-    echo "<td><img src='img/seta_dir.png' height=50px>DEPOIS</td></tr>";
-    echo "</table>";
-    echo "</div>";
     echo "<div id=avisado>";
-    echo "<table border=1 align=center cellspacing=0 spanspacing=0 class=tabela><tr><th>SINISTRO</th>";
+    //echo "<table align=center>";
+    //echo "<tr><td>";
+    echo "<div>ANTES <img src='img/seta_esq.png' height=20px> ";
+    echo "<span> AVISADO 2011 </span>";
+    echo " <img src='img/seta_dir.png' height=20px>";
+    echo " DEPOIS</div>";
+    //echo "</table>";
+    echo "<div class=tabelaEsq>";
+    echo "<table border=1 align=center cellspacing=0 spanspacing=0 ><tr><th>SINISTRO</th>";
     foreach($array as $item){
         echo "<tr><td>$item</td></tr>";
     }
     echo "</table>";
+    echo "</div>";
+    echo "<div class=tabelaDir>";
+    echo "<table border=1 align=center cellspacing=0 spanspacing=0 ><tr><th>SINISTRO</th>";
+    foreach($array as $item){
+        echo "<tr><td>$item</td></tr>";
+    }
+    echo "</table>";
+    echo "</div>";
     echo "</div>";
 ?>
 
